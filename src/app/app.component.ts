85a579fcd3f4ed2266ffc3af1757ea4eb14b1f31
caiolb2000 <caiolb2000@gmail.com>
2024-08-02 14:58:14 +0800

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   standalone: true,
   selector: 'app-root',
-  template: `<h1>Hello World!</h1>`,
+  template: `<h1>Yvan is gay</h1>
+    <p>hello hello hello</p>`,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
